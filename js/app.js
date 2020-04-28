efb14c1d0cf4c6b21c82f2c8aa80d5f21e3fe7c2
Matti <mattibylin@Mattis-MacBook-Air.local>
2020-04-27 19:27:13 -0500

    Se inicia la funcion del timer.

--- js/app.js
+++ js/app.js
@@ -86,3 +86,25 @@ function desplazamiento()
     eliminar=setInterval(function(){eliminarhorver()},150)  //activar funcion eliminarhorver
   }
 }
+function timer()
+{
+  if(seg!=0)
+  {
+    seg=seg-1;
+  }
+  if(seg==0)
+  {
+    if(min==0)
+    {
+      clearInterval(eliminar);
+      clearInterval(newdulces);
+      clearInterval(intervalo);
+      clearInterval(tiempo);
+      $( ".panel-tablero" ).hide("drop","slow",callback);
+      $( ".time" ).hide();
+    }
+    seg=59;
+    min=min-1;
+  }
+  $("#timer").html("0"+min+":"+seg)
+}
