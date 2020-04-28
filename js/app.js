65c58adfd40e8b32ecb83b6c0f61888362d8ec47
Matti <mattibylin@Mattis-MacBook-Air.local>
2020-04-27 19:15:27 -0500

    Se logra poner las librerias y cambiar los colores del titulo tarea uno.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(function(){
+//-----------------item #1-----cambio color titulo------------------------------
+  setInterval(function(){
+    var color=$(".main-titulo").css("color");
+    if(color=="rgb(220, 255, 14)")
+    {
+      $(".main-titulo").css("color","white");
+    }
+    else
+    {
+      $(".main-titulo").css("color","#DCFF0E");
+    }
+  },1000);
+//------------------------------------------------------------------------------
+})
